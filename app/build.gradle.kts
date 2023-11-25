89e3c8919d3d068268c32e7a1d2c7497827e5f97
caio <caiooalexandre94@gmail.com>
2023-11-25 14:38:19 -0300

    iniciando conexao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
     id("kotlin-parcelize")
@@ -44,8 +45,23 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+    //firebase Analytics
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    //Banco de dados
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    //Autenticacao
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    //Armazenamento(fotos, videos,pdf entre outros)
+    implementation("com.google.firebase:firebase-storage-ktx")
+    //picasso(imagens)
+    implementation ("com.squareup.picasso:picasso:2.71828")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     val lifecycle_version = "2.5.0-alpha02"
     //lib Lifecycle
 
@@ -55,19 +71,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 
     //KTX Fragment
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 
     //Dagger Hilt
 
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
-    //retrofit
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation ("com.airbnb.android:lottie:6.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
