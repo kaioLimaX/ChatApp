1ffbb3b984f14e30b8e79d29522f6080ba56cbb6
caio <caiooalexandre94@gmail.com>
2023-11-25 00:20:24 -0300

    configurando constantes e interceptor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -40,6 +43,29 @@ android {
 }
 
 dependencies {
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    val lifecycle_version = "2.5.0-alpha02"
+    //lib Lifecycle
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    //KTX Fragment
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    //Dagger Hilt
+
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    //retrofit
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation ("com.airbnb.android:lottie:6.1.0")
---
+++ app/src/main/java/com/example/chatapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.chatapp.util
+
+object Constants {
+
+    const val BASE_URL = "https://omgvamp-hearthstone-v1.p.rapidapi.com/"
+    const val API_KEY = "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"
+    const val CHAT_MODEL = "gpt-3.5-turbo"
+
+}
