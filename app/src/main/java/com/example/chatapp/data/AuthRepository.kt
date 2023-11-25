017c43070141668473f75a8873044a85d24ed4b6
caio <caiooalexandre94@gmail.com>
2023-11-25 15:11:40 -0300

    criação de interface para autenticação, resource com o progresso da requisição

---
+++ app/src/main/java/com/example/chatapp/data/AuthRepository.kt
@@ -0,0 +1,13 @@
+package com.example.chatapp.data
+
+import com.google.firebase.auth.FirebaseUser
+
+interface AuthRepository {
+
+    val currentUser: FirebaseUser?
+    suspend fun login(email: String, password: String): Resource<FirebaseUser>
+    suspend fun signup(email: String, password: String,name: String): Resource<FirebaseUser>
+
+    fun logout()
+
+}
---
+++ app/src/main/java/com/example/chatapp/data/AuthRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.example.chatapp.data
+
+import com.example.chatapp.data.utils.await
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+
+class AuthRepositoryImpl(
+    private val firebaseAuth: FirebaseAuth
+) : AuthRepository {
+    override val currentUser: FirebaseUser?
+        get() = firebaseAuth.currentUser
+
+    override suspend fun login(email: String, password: String): Resource<FirebaseUser> {
+        return try {
+            val result  = firebaseAuth.signInWithEmailAndPassword(email,password).await()
+            Resource.Sucess(result.user!!)
+
+        }catch (e : Exception){
+            e.printStackTrace()
+            Resource.Failure(e)
+
+        }
+
+    }
+
+    override suspend fun signup(email: String, password: String,name: String): Resource<FirebaseUser> {
+        return try {
+            val result  = firebaseAuth.createUserWithEmailAndPassword(email,password).await()
+            result?.user?.updateProfile(UserProfileChangeRequest.Builder().setDisplayName(name).build())
+            Resource.Sucess(result.user!!)
+
+        }catch (e : Exception){
+            e.printStackTrace()
+            Resource.Failure(e)
+
+        }
+    }
+
+    override fun logout() {
+        TODO("Not yet implemented")
+    }
+}
