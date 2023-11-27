35e8461f803a42b665507853f2e2774bbc93247c
caio <caiooalexandre94@gmail.com>
2023-11-27 14:05:24 -0300

    alterado StateFlow para livedata para facil observação do viewModel

--- app/src/main/java/com/example/chatapp/view/ui/auth/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/chatapp/view/ui/auth/viewmodels/LoginViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.chatapp.view.ui.auth.viewmodels
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.chatapp.data.AuthRepository
@@ -17,29 +19,31 @@ class LoginViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
-    private val _loginFlow = MutableStateFlow<Resource<FirebaseUser>?>(null)
+    private val _loginFlow = MutableLiveData<Resource<FirebaseUser>?>()
 
-    val loginFlow : StateFlow<Resource<FirebaseUser>?> = _loginFlow
+    val loginFlow: LiveData<Resource<FirebaseUser>?>
+        get() = _loginFlow
 
     val currentUser: FirebaseUser?
         get() = authRepository.currentUser
 
     init {
-        if(authRepository.currentUser != null){
+        if (authRepository.currentUser != null) {
             _loginFlow.value = Resource.Sucess(authRepository.currentUser!!)
-            Log.i("info_auth", ": ${Resource.Sucess(authRepository.currentUser!!)} ")
+            Log.i("info_auth", "${Resource.Sucess(authRepository.currentUser!!)} ")
         }
     }
 
     fun onClickLogin(
         email: String,
-        password: String  ) = viewModelScope.launch {
-        _loginFlow.value = Resource.Loading
-           val response =  authRepository.login(email,password)
-        _loginFlow.value = response
+        password: String
+    ) = viewModelScope.launch {
+        _loginFlow.postValue(Resource.Loading)
+        val response = authRepository.login(email, password)
+        _loginFlow.postValue(response)
+        Log.i("info_response", "$_loginFlow")
 
     }
-
 
 
 }
