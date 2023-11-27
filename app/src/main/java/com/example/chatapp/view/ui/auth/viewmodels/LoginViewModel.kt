4994bec9a8c1bac15ed0d0b23190e7bfdebaec57
caio <caiooalexandre94@gmail.com>
2023-11-27 00:29:34 -0300

    iniciado viewModelLogin

--- app/src/main/java/com/example/chatapp/view/ui/auth/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/chatapp/view/ui/auth/viewmodels/LoginViewModel.kt
@@ -1,14 +1,47 @@
 package com.example.chatapp.view.ui.auth.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.chatapp.data.AuthRepository
+import com.example.chatapp.data.Resource
+import com.google.firebase.auth.FirebaseUser
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class LoginViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
-    suspend fun onClickLogin(email : String, password : String){
-        authRepository.login(email,password)
+    private val _loginFlow = MutableStateFlow<Resource<FirebaseUser>?>(null)
+
+    val loginFlow : StateFlow<Resource<FirebaseUser>?> = _loginFlow
+
+    val currentUser: FirebaseUser?
+        get() = authRepository.currentUser
+
+    init {
+        if(authRepository.currentUser != null){
+            _loginFlow.value = Resource.Sucess(authRepository.currentUser!!)
+            Log.i("info_auth", ": ${Resource.Sucess(authRepository.currentUser!!)} ")
+        }
     }
+
+    fun onClickLogin(
+        email: String,
+        password: String  ) = viewModelScope.launch {
+        _loginFlow.value = Resource.Loading
+           val response =  authRepository.login(email,password)
+        _loginFlow.value = response
+
+    }
+
+
+
 }
+
+
