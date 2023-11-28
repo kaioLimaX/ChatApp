92255a5fa4d8ce4a9b0345f2835a0e83d0080c25
caio <caiooalexandre94@gmail.com>
2023-11-28 13:26:37 -0300

    adicionado retorno sealed para controlar erro do Name

--- app/src/main/java/com/example/chatapp/view/ui/auth/fragments/Validation.kt
+++ app/src/main/java/com/example/chatapp/view/ui/auth/fragments/Validation.kt
@@ -3,7 +3,7 @@ package com.example.chatapp.view.ui.auth.fragments
 import java.lang.Exception
 
 sealed class ValidationResult {
-    object Success : ValidationResult()
     data class ErrorEmail(val message: String) : ValidationResult()
     data class ErrorPassword(val message: String) : ValidationResult()
+    data class ErrorName(val message: String) : ValidationResult()
 }
