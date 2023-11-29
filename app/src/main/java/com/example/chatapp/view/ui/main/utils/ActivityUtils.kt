b9196ea63d8beae151ab5dca894c1f35d15a49d1
caio <caiooalexandre94@gmail.com>
2023-11-28 22:11:56 -0300

    adicionado funções adicionais para navegar entre activitys

--- app/src/main/java/com/example/chatapp/view/ui/main/utils/ActivityUtils.kt
+++ app/src/main/java/com/example/chatapp/view/ui/main/utils/ActivityUtils.kt
@@ -1,7 +1,9 @@
 package com.example.chatapp.view.ui.main.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 
 class ActivityUtils {
 
@@ -10,6 +12,28 @@ class ActivityUtils {
         fun goToActivity(context: Context, activityClass: Class<*>) {
             val intent = Intent(context, activityClass)
             context.startActivity(intent)
+        }
+        fun goToActivity(context: Context, activityClass: Class<*>,condition: Boolean) {
+            val intent = Intent(context, activityClass)
+            context.startActivity(intent)
+            if (context is Activity) {
+                context.finish()
+            }
+
+        }
+        fun goToActivity(context: Context,activityClass: Class<*>, extras: Bundle? = null) {
+            val intent = Intent(context, activityClass)
+
+            // Adiciona os extras à intent, se fornecidos
+            if (extras != null) {
+                intent.putExtras(extras)
+            }
+
+            context.startActivity(intent)
+
+            if (context is Activity) {
+                context.finish()
+            }
         }
     }
 }
